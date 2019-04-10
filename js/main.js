5ae8ad8c021aa1295e813c8e9635b8a00a041cf4
vj <viju1985@gmail.com>
2019-04-10 22:14:17 +0800

    README.md

--- js/main.js
+++ js/main.js
@@ -15,12 +15,19 @@ $(document).ready(function () {
         { "organizer": "Swo", "duration": "150" },
         { "organizer": "Tgb", "duration": "180" },
         { "organizer": "Okm", "duration": "240" }
-    ]
-  
-    var meetingRooms = [{ room: [], durationAvaialble: [180, 240] }, { room: [], durationAvaialble: [180, 240] }, { room: [], durationAvaialble: [180, 240] }, { room: [], durationAvaialble: [180, 240] }, { room: [], durationAvaialble: [180, 240] }, { room: [], durationAvaialble: [180, 240] }];
-
-
-    let sortedMeetings = meetings.slice(0).sort((a, b) => b.duration - a.duration);
+    ],
+        meetingRooms = [
+            { room: [], durationAvaialble: [180, 240] },
+            { room: [], durationAvaialble: [180, 240] },
+            { room: [], durationAvaialble: [180, 240] },
+            { room: [], durationAvaialble: [180, 240] },
+            { room: [], durationAvaialble: [180, 240] },
+            { room: [], durationAvaialble: [180, 240] }
+        ],
+        morningStart = 9,
+        afternoonStart = 1;
+    
+        let sortedMeetings = meetings.slice(0).sort((a, b) => b.duration - a.duration);
 
 
     $.each(sortedMeetings, function (index, value) {
@@ -28,11 +35,27 @@ $(document).ready(function () {
         $.each(meetingRooms, function (index, rooms) {
 
             if (value.duration <= rooms.durationAvaialble[0]) {
-                rooms.durationAvaialble[0] = rooms.durationAvaialble[0] - value.duration;
+                if(rooms.durationAvaialble[0] == 180){
+                    value["startTime"]=convertMinutesToHHMM(morningStart*60);
+                    value["endTime"]=convertMinutesToHHMM((morningStart*60)+Number(value.duration));
+                }else if(rooms.durationAvaialble[0] != 180){
+                    var tempStartTime=(morningStart*60)+(180-rooms.durationAvaialble[0]);
+                    value["startTime"]=convertMinutesToHHMM(tempStartTime);
+                    value["endTime"]=convertMinutesToHHMM(tempStartTime+Number(value.duration));
+                }
+                rooms.durationAvaialble[0] = rooms.durationAvaialble[0] - Number(value.duration);;
                 value["time"] = "Morning";
                 rooms.room.push(value);
                 return false;
             } else if (value.duration <= rooms.durationAvaialble[1]) {
+                if(rooms.durationAvaialble[1] == 240){
+                    value["startTime"]=convertMinutesToHHMM(afternoonStart*60);
+                    value["endTime"]=convertMinutesToHHMM((afternoonStart*60)+Number(value.duration));
+                }else if(rooms.durationAvaialble[1] != 240){
+                    var tempStartTime=(afternoonStart*60)+(240-rooms.durationAvaialble[1]);
+                    value["startTime"]=convertMinutesToHHMM(tempStartTime);
+                    value["endTime"]=convertMinutesToHHMM(tempStartTime+Number(value.duration));
+                }
                 rooms.durationAvaialble[1] = rooms.durationAvaialble[1] - value.duration;
                 value["time"] = "Afternoon";
                 rooms.room.push(value);
@@ -44,16 +67,32 @@ $(document).ready(function () {
         })
 
     });
-
+    
     //display Logic
     $.each(meetingRooms, function (index, value) {
-        if(value.room.length != 0){
-            $(".meeting").append("<h4>MEETING ROOM "+(index+1)+"</h4>")
+        if (value.room.length != 0) {
+            $(".meeting").append("<h4>MEETING ROOM " + (index + 1) + "</h4>")
+            var tempMeetingRoom=value.room;
+            tempMeetingRoom = tempMeetingRoom.sort(function(a, b){
+                if(a.time < b.time) { return 1; }
+                if(a.time > b.time) { return -1; }
+                return 0;
+            });
             $.each(value.room, function (index, rooms) {
-                $(".meeting").append('<div class="alert alert-primary" role="alert">Organizer : '+rooms.organizer+'<br>Duration : '+rooms.duration+'<br>Session Time : '+rooms.time+'</div>')                
-
+                $(".meeting").append('<div class="alert alert-primary" role="alert">Organizer : '
+                 + rooms.organizer + '<br>Timing : ' 
+                 + rooms.startTime+" - "+rooms.endTime+'<br>Duration : '
+                 + rooms.duration + '<br>Session : ' 
+                 + rooms.time + '</div>');
             });
         }
-    });
+    }); 
 
 });
+function convertMinutesToHHMM (minutes){
+    var h = Math.floor(minutes / 60);
+    var m = minutes % 60;
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    return h + ':' + m;
+}
